f68bb46a73ff2c0112ca7f98542602724c01fd83
Matheus Lima <matheeus161@gmail.com>
2026-03-11 20:57:07 -0300

    refactor: PaymentValidatorTest to use parameterized tests source

--- src/test/java/com/eliasnogueira/paymentservice/unit/PaymentValidatorTest.java
+++ src/test/java/com/eliasnogueira/paymentservice/unit/PaymentValidatorTest.java
@@ -3,8 +3,12 @@ package com.eliasnogueira.paymentservice.unit;
 import com.eliasnogueira.paymentservice.validator.PaymentLimitValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,5 +39,33 @@ class PaymentValidatorTest {
     @DisplayName("Payment should be successful when amount is null")
     void shouldNotAcceptANullValue() {
         assertThat(PaymentLimitValidator.isWithinLimit(null)).isFalse();
+    }
+
+    @ParameterizedTest(name = "Payment should not be successful when value is ${0}")
+    @MethodSource("edgeCasesForLimit")
+    void shouldNotAcceptEdgeCases(BigDecimal amount) {
+        assertThat(PaymentLimitValidator.isWithinLimit(amount)).isFalse();
+    }
+
+    static Stream<Arguments> edgeCasesForLimit() {
+        return Stream.of(
+                Arguments.arguments(new BigDecimal("00.00")),
+                Arguments.arguments(new BigDecimal("2000.01")),
+                Arguments.arguments(new BigDecimal("3500.00"))
+        );
+    }
+
+    @ParameterizedTest(name = "Payment should be successful when value is ${0}")
+    @MethodSource("happyPathsForLimit")
+    void shouldAcceptAmountUnderTheLimit(BigDecimal amount) {
+        assertThat(PaymentLimitValidator.isWithinLimit(amount)).isTrue();
+    }
+
+    static Stream<Arguments> happyPathsForLimit() {
+        return Stream.of(
+                Arguments.arguments(new BigDecimal("00.1")),
+                Arguments.arguments(new BigDecimal("1999.99")),
+                Arguments.arguments(new BigDecimal("2000.00"))
+        );
     }
 }
